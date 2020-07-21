23a7dd12a0dc8cb4b6c40ba04817d9078ea39f76
ex7763 <eddie55020@gmail.com>
2020-07-21 10:23:09 +0800

    add imu and simple keyboard control

--- simulation/controllers/my_controller/my_controller.py
+++ simulation/controllers/my_controller/my_controller.py
@@ -4,8 +4,14 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 from controller import Camera
+from controller import Keyboard
+
+from controller import InertialUnit
+from controller import Gyro
+
 
 import math
+import time
 
 # create the Robot instance.
 robot = Robot()
@@ -43,23 +49,81 @@ motor_lst = [
 #  ds = robot.getDistanceSensor('dsname')
 #  ds.enable(timestep)
 
+def move_forward():
+    motor_lst[1 + 0*3].setPosition(math.pi * -1 / 8)
+    motor_lst[1 + 0*3].setVelocity(1.0)
+    
+def rotate(angle):
+    for i in range(6):
+        motor_lst[0 + i*3].setPosition(angle)
+        motor_lst[0 + i*3].setVelocity(1.0)
+
 camera = Camera("camera_d435i")
 camera.enable(15)
 print(camera.getSamplingPeriod())
 camera.saveImage("~/test.png", 100)
 
+gyro = Gyro("gyro")
+gyro.enable(60)
+
+inertial_unit = InertialUnit("inertial_unit")
+inertial_unit.enable(60)
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-
-    #motor_lst[0].setPosition(10.0)
-    #motor_lst[0].setVelocity(1.0)
+def default_low_pos():
     for i in range(6):
+        motor_lst[0 + i*3].setPosition(0)
+        motor_lst[0 + i*3].setVelocity(1.0)
+    
+        motor_lst[1 + i*3].setPosition(math.pi * 1 / 8)
+        motor_lst[1 + i*3].setVelocity(1.0)
+        
+        motor_lst[2 + i*3].setPosition(math.pi * -6 / 8)
+        motor_lst[2 + i*3].setVelocity(1.0)
+        
+def default_high_pos():
+    for i in range(6):
+        motor_lst[0 + i*3].setPosition(0)
+        motor_lst[0 + i*3].setVelocity(1.0)
+        
+        motor_lst[2 + i*3].setPosition(math.pi * -1 / 8)
+        motor_lst[2 + i*3].setVelocity(1.0)
     
         motor_lst[1 + i*3].setPosition(math.pi * -3 / 8)
         motor_lst[1 + i*3].setVelocity(1.0)
         
-        motor_lst[2 + i*3].setPosition(math.pi * -1 / 8)
-        motor_lst[2 + i*3].setVelocity(1.0)
+        
+
+default_high_pos()
+
+keyboard = Keyboard()
+keyboard.enable(60)
+
+while robot.step(timestep) != -1:
+
+    #motor_lst[0].setPosition(10.0)
+    #motor_lst[0].setVelocity(1.0)
+
+        
+    key=keyboard.getKey()
+    if (key==ord('D')):
+        print('D is pressed')
+        rotate(math.pi * -2 / 8)
+    elif (key==ord('F')):
+        print('F is pressed')
+        rotate(math.pi * 2 / 8)
+    elif (key==ord(' ')):
+        print('Space is pressed')
+        default_high_pos()
+    elif (key==ord('V')):
+        print('V is pressed')
+        default_low_pos()
+        
+    print("gyro", gyro.getValues())
+    print("inertial_unit", inertial_unit.getRollPitchYaw())
+        
+    #rotate()
+    #move_forward()
 
 # Enter here exit cleanup code.
